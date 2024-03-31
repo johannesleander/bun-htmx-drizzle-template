52e5a09ce9df5dd16db6ce1103d02704247459b8
Johannes Leander <leanderjohannes@gmail.com>
2024-03-31 08:30:58 +0200

    prettier style default

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,18 @@
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 120,
+  proseWrap: "always",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 4,
+  trailingComma: "all",
+  useTabs: false,
 };

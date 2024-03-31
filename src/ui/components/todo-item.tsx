21cd6cb234b74262bef4c918f00325f07ce111a6
Johannes Leander <leanderjohannes@gmail.com>
2024-03-31 08:26:11 +0200

    prettierrc

--- src/ui/components/todo-item.tsx
+++ src/ui/components/todo-item.tsx
@@ -1,30 +1,30 @@
 import { Todo } from "@db/schema";
 
 export default function TodoItem({ id, content, timestamp }: Todo) {
-    return (
-        <li key={id} class="py-3 sm:py-4">
-            <div class="flex items-center">
-                <div class="flex-1 min-w-0 ms-4">
-                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                        {content}
-                    </p>
-                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                        {timestamp}
-                    </p>
-                </div>
-                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                    <button
-                        name="todoId"
-                        value={id}
-                        hx-delete="/api/todo"
-                        hx-swap="delete"
-                        hx-ext="json-enc"
-                        hx-trigger="click"
-                    >
-                        ❌
-                    </button>
-                </div>
-            </div>
-        </li>
-    );
+  return (
+    <li key={id} class="py-3 sm:py-4">
+      <div class="flex items-center">
+        <div class="ms-4 min-w-0 flex-1">
+          <p class="truncate text-sm font-medium text-gray-900 dark:text-white">
+            {content}
+          </p>
+          <p class="truncate text-sm text-gray-500 dark:text-gray-400">
+            {timestamp}
+          </p>
+        </div>
+        <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+          <button
+            name="todoId"
+            value={id}
+            hx-delete="/api/todo"
+            hx-swap="delete"
+            hx-ext="json-enc"
+            hx-trigger="click"
+          >
+            ❌
+          </button>
+        </div>
+      </div>
+    </li>
+  );
 }

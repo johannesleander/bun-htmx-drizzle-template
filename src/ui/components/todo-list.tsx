9c88aac304734b7e9b4abbaf8ad8f9e7da6c31b8
Johannes Leander <leanderjohannes@gmail.com>
2024-03-31 08:36:50 +0200

    chore: Cleans up unused todo-list styles

--- src/ui/components/todo-list.tsx
+++ src/ui/components/todo-list.tsx
@@ -1,18 +1,18 @@
 export default function TodoList() {
     return (
-        <div class="mt-8 w-full max-w-m dark:text-white">
-            <h1>My Todos</h1>
-            <div class="mt-2 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-2 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-2 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                <div class="flow-root">
-                    <ul
-                        hx-get="/api/todos"
-                        hx-trigger="load, todo-delete from:body"
-                        id="todo-list"
-                        role="list"
-                        class="divide-y divide-gray-200 dark:divide-gray-700"
-                    ></ul>
-                </div>
-            </div>
+      <div class="max-w-m mt-8 w-full dark:text-white">
+        <h1>My Todos</h1>
+        <div class="mt-2 rounded-lg border border-gray-200 bg-white p-4 shadow sm:p-8 dark:border-gray-700 dark:bg-gray-800">
+          <div class="flow-root">
+            <ul
+              hx-get="/api/todos"
+              hx-trigger="load, todo-delete from:body"
+              id="todo-list"
+              role="list"
+              class="divide-y divide-gray-200 dark:divide-gray-700"
+            ></ul>
+          </div>
         </div>
+      </div>
     );
 }
